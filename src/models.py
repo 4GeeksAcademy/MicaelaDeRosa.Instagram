45fd5d5d6c545538d116e761396b2e91172be518
MicaelaDeRosa23 <micaaderosa@gmail.com>
2024-11-20 04:11:04 +0000

    MicaelaDeRosa.Instagram

--- src/models.py
+++ src/models.py
@@ -1,52 +1,59 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, 
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
+from datetime import datetime
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Username(Base):
+  __tablename__ = 'username'
+  id = Column(Integer, primary_key=True)
+  username = Column(String(20), nullable=False)
+  name = Column(String(25), nullable=False)
+  bio = Column(String(250), nullable=True)
+  email = Column(String(200), nullable=False)
+  password = Column(String(20), nullable=False)
+  phone_number = Column(Integer, nullable=False)
+  
+class Story(Base):
+  __tablename__ = 'story'
+  id = Column(Integer, primary_key=True)
+  views = Column(Integer, nullable=False)
+  caption = Column(String(250), nullable=True)
+  shares = Column(Integer, nullable=True)
+  media_url = Column(String(80))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Post(Base):
+  __tablename__= 'post'
+  id = Column(Integer, primary_key=True)
+  photo_id = Column(Integer, nullable=False)
+  caption = Column(String(250), nullable=True)
+  likes = Column(Integer, nullable=True)
+  comment = Column(String(250), nullable=True)
+  shares = Column(Integer)
 
-    def to_dict(self):
-        return {}
+class Followers(Base):
+  __tablename__= 'followers'
+  id = Column(Integer, primary_key=True)
+  name = Column(String(25), nullable=False)
+  email = Column(String(200), nullable=False)
+  password = Column(String(20), nullable=False)
+  bio = Column(String(250))
+  phone_number = Column(Integer, nullable=False)
+
+class User(Base):
+  __tablename__= 'user'
+  id = Column(Integer, primary_key=True)
+  username_id = Column(Integer, ForeignKey('username.id'))
+  story_id = Column(Integer, ForeignKey('story.id'))
+  post_id = Column(Integer, ForeignKey('post.id'))
+  followers_id = Column(Integer, ForeignKey('followers.id'))
+  
 
 ## Draw from SQLAlchemy base
-class User(Base):
-    __tablename__="user"
-    id = Column(Integer,primary_key=True)
-    username = Column (String(30), nullable=False)
-    firstname =Column (String(30))
-    lastame =Column (String(30))
-    email = Column(String(40) , nullable=False ,unique=True)
-    comments=relationship("Comment",back_populates="author_realtionship")
-
-
-class Comment(Base):
-    __tablename__="comment"
-    id = Column(Integer,primary_key=True)
-    comment_text = Column (String(200), nullable=False)
-    author=Column(Integer,ForeignKey("user.id"))
-    author_relationship=relationship("User",back_populates="comments")
-
-
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
