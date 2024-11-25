007dd5f29a4e234ffd27bef3b8f9891843beec54
MicaelaDeRosa23 <micaaderosa@gmail.com>
2024-11-25 03:48:44 +0000

    MicaelaDeRosa.Instagram

--- src/models.py
+++ src/models.py
@@ -1,57 +1,59 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, 
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-from datetime import datetime
-from eralchemy2 import render_er
+from eralchemy import render_er
 
 Base = declarative_base()
 
-class Username(Base):
-  __tablename__ = 'username'
-  id = Column(Integer, primary_key=True)
-  username = Column(String(20), nullable=False)
-  name = Column(String(25), nullable=False)
-  bio = Column(String(250), nullable=True)
-  email = Column(String(200), nullable=False)
-  password = Column(String(20), nullable=False)
-  phone_number = Column(Integer, nullable=False)
-  
-class Story(Base):
-  __tablename__ = 'story'
-  id = Column(Integer, primary_key=True)
-  views = Column(Integer, nullable=False)
-  caption = Column(String(250), nullable=True)
-  shares = Column(Integer, nullable=True)
-  media_url = Column(String(80))
+class Comment(Base):
+    __tablename__ = 'comment'
+    
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Media(Base):
+    __tablename__ = 'media'
+
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id')) 
 
 class Post(Base):
-  __tablename__= 'post'
-  id = Column(Integer, primary_key=True)
-  photo_id = Column(Integer, nullable=False)
-  caption = Column(String(250), nullable=True)
-  likes = Column(Integer, nullable=True)
-  comment = Column(String(250), nullable=True)
-  shares = Column(Integer)
+    __tablename__ = 'post'
+    
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id')) 
+    comment = relationship(Comment)
+    media = relationship(Media)
 
-class Followers(Base):
-  __tablename__= 'followers'
-  id = Column(Integer, primary_key=True)
-  name = Column(String(25), nullable=False)
-  email = Column(String(200), nullable=False)
-  password = Column(String(20), nullable=False)
-  bio = Column(String(250))
-  phone_number = Column(Integer, nullable=False)
+class Follower(Base):
+    __tablename__ = 'follower'
+
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id =  Column(Integer, ForeignKey('user.id'))
 
 class User(Base):
-  __tablename__= 'user'
-  id = Column(Integer, primary_key=True)
-  username_id = Column(Integer, ForeignKey('username.id'))
-  story_id = Column(Integer, ForeignKey('story.id'))
-  post_id = Column(Integer, ForeignKey('post.id'))
-  followers_id = Column(Integer, ForeignKey('followers.id'))
+    __tablename__ = 'user'
   
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    firtsname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    post = relationship(Post)
+    comment = relationship(Comment)
+    media = relationship(Media)
+    follower = relationship(Follower)
+
+    def to_dict(self):
+        return {}
 
 ## Draw from SQLAlchemy base
 try:
@@ -59,4 +61,7 @@ try:
     print("Success! Check the diagram.png file")
 except Exception as e:
     print("There was a problem genering the diagram")
-    raise e
+    raise 
+
+
+
